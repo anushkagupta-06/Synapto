4b59e2a6af1a6483a9a6738101a3b0636edb9f49
MNNITAK <ay9120991471@gmail.com>
2025-06-29 22:27:54 +0530

    used disc storage for file storage using multer

--- Synapto/server/routes/pdfRoutes.js
+++ Synapto/server/routes/pdfRoutes.js
@@ -2,19 +2,33 @@ import express from 'express';
 import  {Summary}  from '../controllers/Summary.js';
 import multer from "multer";
 import fs from 'fs';
-
-
-
 const router = express.Router();
 // const uploadDir = './summary';
 // if (!fs.existsSync(uploadDir)) {
 //   fs.mkdirSync(uploadDir);
 // }
-const upload = multer({ dest: "summary/" });
 
+// const upload = multer({ dest: "summary/" });
+const storage = multer.diskStorage({
+  destination: 'summary/',
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
 
-
-
+const upload = multer({ 
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype === 'application/pdf') {
+      cb(null, true);
+    } else {
+      cb(new Error('Only PDF files are allowed!'), false);
+    }
+  },
+  limits: {
+    fileSize: 20 * 1024 * 1024 // 10MB limit
+  }
+});
 router.post("/summary", upload.single("file"), Summary);
 
 export default router;
