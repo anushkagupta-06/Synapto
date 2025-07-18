9e01ed2c2d9a3adbf8ca564ee8d95813db00621b
MNNITAK <ay9120991471@gmail.com>
2025-07-18 14:42:34 +0530

    added main transcribing utility

---
+++ server/utils/transcribeAudio.js
@@ -0,0 +1,15 @@
+// import { OpenAI } from "openai";
+// import fs from "fs";
+
+// const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+// export const transcribeAudio = async (audioPath) => {
+//   const transcript = await openai.audio.transcriptions.create({
+//     file: fs.createReadStream(audioPath),
+//     model: "whisper-1"
+//   });
+
+//   return transcript.text;
+// };
+
+
