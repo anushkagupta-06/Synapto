4da3a1b65274fbeee38f6dce7158a38c916303f5
Anushka Gupta <hey.anushka.gupta@gmail.com>
2025-07-20 03:16:06 +0530

    Fix: Rename HomePage.css to Homepage.css for correct import

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
-import "./HomePage.css";
+import "./Homepage.css";
 import Spline from "@splinetool/react-spline";
 import { useAuth } from "../context/contextapi";
 import {
